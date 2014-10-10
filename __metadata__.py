7fb2177ee6ab775eff7760452dc7e06a37e9593b
Trevor Bača <trevorbaca@gmail.com>
2014-10-10 10:44:48 -0400

    Set up score.

---
+++ __metadata__.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+import collections
+
+
+metadata = collections.OrderedDict([
+    ('catalog_number', 'AWN-018'),
+    ('forces_tagline', 'for piano'),
+    ('paper_dimensions', '17 x 11 in'),
+    ('price', '\\euro 30 / \\$38'),
+    ('title', 'Mr\xc3\xa1z'),
+    ('year', 2015),
+    ])
