a28201cdbc6feac905882570b819fc5230a31b3c
Trevor Bača <trevorbaca@gmail.com>
2015-09-09 11:29:38 -0400

    Updated IDE package path.

--- mraz/test/test_mraz_segments.py
+++ mraz/test/test_mraz_segments.py
@@ -7,7 +7,7 @@ import mraz
 from abjad.tools import systemtools
 
 
-boilerplate_path = ide.idetools.Configuration().boilerplate_directory
+boilerplate_path = ide.tools.idetools.Configuration().boilerplate_directory
 boilerplate_path = os.path.join(boilerplate_path, '__illustrate_segment__.py')
 
 segments_path = os.path.join(mraz.__path__[0], 'segments')
