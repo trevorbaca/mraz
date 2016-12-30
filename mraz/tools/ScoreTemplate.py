1df653a1398628b27eb05e6154aae64d50683128
Trevor Bača <trevor.baca@gmail.com>
2016-12-30 14:23:13 -0600

    Segment 7.

--- mraz/tools/ScoreTemplate.py
+++ mraz/tools/ScoreTemplate.py
@@ -5,6 +5,11 @@ import baca
 
 class ScoreTemplate(baca.tools.ScoreTemplate):
     r'''Score template.
+
+    ::
+
+        >>> import mraz
+
     '''
 
     ### CLASS VARIABLES ###
@@ -23,11 +28,7 @@ class ScoreTemplate(baca.tools.ScoreTemplate):
 
         ..  container:: example
 
-            **Example.** Calls score template:
-
-            ::
-
-                >>> import mraz
+            Calls score template:
 
             ::
 
