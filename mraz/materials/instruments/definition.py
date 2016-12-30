02f88e8f37094c93e1c3bfb6b28ddfa689a6c229
Trevor Bača <trevor.baca@gmail.com>
2016-12-30 16:41:28 -0600

    Segment 7.

--- mraz/materials/instruments/definition.py
+++ mraz/materials/instruments/definition.py
@@ -3,13 +3,16 @@ import abjad
 import baca
 
 
+piano = abjad.instrumenttools.Piano(
+    instrument_name_markup=baca.markup.make_instrument_name_markup(
+        'Piano'),
+    short_instrument_name_markup=abjad.markuptools.Markup.null(),
+    )
+piano._default_scope = 'PianoMusicStaffGroup'
+
 instruments = abjad.datastructuretools.TypedOrderedDict([
     (
         'piano', 
-        abjad.instrumenttools.Piano(
-            instrument_name_markup=baca.markup.make_instrument_name_markup(
-                'Piano'),
-            short_instrument_name_markup=abjad.markuptools.Markup.null(),
-            ),
+        piano,
         ),
     ])
