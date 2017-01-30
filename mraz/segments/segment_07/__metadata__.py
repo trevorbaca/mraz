4c1362492ff0d4e0e16a5ceb9a6acbccb255d116
Trevor Bača <trevor.baca@gmail.com>
2017-01-30 14:31:32 -0600

    Segment 7.

--- mraz/segments/segment_07/__metadata__.py
+++ mraz/segments/segment_07/__metadata__.py
@@ -22,7 +22,7 @@ metadata = abjad.datastructuretools.TypedOrderedDict(
                 ),
             ),
         ('end_tempo', 112),
-        ('end_time_signature', '17/16'),
+        ('end_time_signature', '10/16'),
         ('first_bar_number', 3),
         ('measure_count', 16),
         ('segment_count', 3),
