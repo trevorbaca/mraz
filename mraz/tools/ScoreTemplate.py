484a125f1717826fc8576d7a84b79de85432435d
Trevor Bača <trevor.baca@gmail.com>
2017-01-31 19:46:13 -0600

    Segment 7.

--- mraz/tools/ScoreTemplate.py
+++ mraz/tools/ScoreTemplate.py
@@ -12,15 +12,6 @@ class ScoreTemplate(baca.tools.ScoreTemplate):
 
     '''
 
-    ### CLASS VARIABLES ###
-
-    voice_abbreviations = {
-        'v1': 'Piano Music Voice 1',
-        'v2': 'Piano Music Voice 2',
-        'v3': 'Piano Music Voice 3',
-        'v4': 'Piano Music Voice 4',
-        }
-
     ### SPECIAL METHODS ###
 
     def __call__(self):
