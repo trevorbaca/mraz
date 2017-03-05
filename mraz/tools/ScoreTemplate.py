4026d9c6fb73748233aa809d0e778ac61e88c09b
Trevor Bača <trevor.baca@gmail.com>
2017-03-05 15:49:10 -0600

    Segment 4.4.

--- mraz/tools/ScoreTemplate.py
+++ mraz/tools/ScoreTemplate.py
@@ -84,7 +84,13 @@ class ScoreTemplate(baca.tools.ScoreTemplate):
         'RH Voice 2I': 'black',
         'RH Voice 3': 'darkgreen',
         'RH Voice 3I': 'darkgreen',
+        'RH Voice 4': 'blue',
+        'RH Voice 5': 'darkmagenta',
+        'RH Voice 6': 'darkcyan',
         'RH Resonance Voice': 'darkred',
+        'LH Voice 1': 'red',
+        'LH Voice 2': 'black',
+        'LH Voice 3': 'darkgreen',
         'LH Voice 4': 'blue',
         'LH Voice 4I': 'blue',
         'LH Voice 5': 'darkmagenta',
@@ -103,72 +109,72 @@ class ScoreTemplate(baca.tools.ScoreTemplate):
         '''
         import mraz
         time_signature_context = self._make_time_signature_context()
-        piano_music_voice_1 = abjad.scoretools.Voice(
+        piano_music_voice_1 = abjad.Voice(
             [], 
             context_name='RHVoiceOne',
             name='RH Voice 1',
             )
-        piano_music_voice_1I = abjad.scoretools.Voice(
+        piano_music_voice_1I = abjad.Voice(
             [], 
             context_name='RHVoiceOneInserts',
             name='RH Voice 1I',
             )
-        piano_music_voice_2 = abjad.scoretools.Voice(
+        piano_music_voice_2 = abjad.Voice(
             [], 
             context_name='RHVoiceTwo',
             name='RH Voice 2',
             )
-        piano_music_voice_2I = abjad.scoretools.Voice(
+        piano_music_voice_2I = abjad.Voice(
             [], 
             context_name='RHVoiceTwoInserts',
             name='RH Voice 2I',
             )
-        piano_music_voice_3 = abjad.scoretools.Voice(
+        piano_music_voice_3 = abjad.Voice(
             [], 
             context_name='RHVoiceThree',
             name='RH Voice 3',
             )
-        piano_music_voice_3I = abjad.scoretools.Voice(
+        piano_music_voice_3I = abjad.Voice(
             [], 
             context_name='RHVoiceThreeInserts',
             name='RH Voice 3I',
             )
-        piano_music_voice_rh_resonance = abjad.scoretools.Voice(
+        piano_music_voice_rh_resonance = abjad.Voice(
             [], 
             context_name='RHResonanceVoice',
             name='RH Resonance Voice',
             )
-        piano_music_voice_4 = abjad.scoretools.Voice(
+        piano_music_voice_4 = abjad.Voice(
             [], 
             context_name='LHVoiceFour',
             name='LH Voice 4',
             )
-        piano_music_voice_4I = abjad.scoretools.Voice(
+        piano_music_voice_4I = abjad.Voice(
             [], 
             context_name='LHVoiceFourInserts',
             name='LH Voice 4I',
             )
-        piano_music_voice_5 = abjad.scoretools.Voice(
+        piano_music_voice_5 = abjad.Voice(
             [], 
             context_name='LHVoiceFive',
             name='LH Voice 5',
             )
-        piano_music_voice_5I = abjad.scoretools.Voice(
+        piano_music_voice_5I = abjad.Voice(
             [], 
             context_name='LHVoiceFiveInserts',
             name='LH Voice 5I',
             )
-        piano_music_voice_6 = abjad.scoretools.Voice(
+        piano_music_voice_6 = abjad.Voice(
             [], 
             context_name='LHVoiceSix',
             name='LH Voice 6',
             )
-        piano_music_voice_6I = abjad.scoretools.Voice(
+        piano_music_voice_6I = abjad.Voice(
             [], 
             context_name='LHVoiceSixInserts',
             name='LH Voice 6I',
             )
-        piano_music_voice_lh_resonance = abjad.scoretools.Voice(
+        piano_music_voice_lh_resonance = abjad.Voice(
             [], 
             context_name='LHResonanceVoice',
             name='LH Resonance Voice',
@@ -211,14 +217,14 @@ class ScoreTemplate(baca.tools.ScoreTemplate):
             )
         piano = mraz.materials.instruments['piano']
         abjad.attach(piano, piano_music_staff_group)
-        music_context = abjad.scoretools.Context(
+        music_context = abjad.Context(
             [
                 piano_music_staff_group,
             ],
             context_name='MusicContext',
             name='Music Context',
             )
-        score = abjad.scoretools.Score(
+        score = abjad.Score(
             [
             time_signature_context,
             music_context,
