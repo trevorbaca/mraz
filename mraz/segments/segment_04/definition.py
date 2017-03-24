6ba4152df7508b127957eef75e854fb2354fac19
Trevor Bača <trevor.baca@gmail.com>
2017-03-23 18:35:56 -0700

    Integrating CollectionManager.

--- mraz/segments/segment_04/definition.py
+++ mraz/segments/segment_04/definition.py
@@ -10,7 +10,7 @@ import mraz
 
 accumulator = mraz.tools.MusicAccumulator(mraz.tools.ScoreTemplate())
 collection_maker = mraz.tools.CollectionMaker()
-collections = collection_maker.make_stage_4_collections()
+collections = collection_maker.make_segment_4_collections()
 
 #################################### [4.1] ####################################
 
