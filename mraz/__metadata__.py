8edd6ea32398e07947ba4c2d48b31581cb5a15fe
Trevor Bača <trevor.baca@gmail.com>
2017-04-20 15:21:40 -0500

    Added metadata.

--- mraz/__metadata__.py
+++ mraz/__metadata__.py
@@ -4,6 +4,7 @@ import abjad
 
 metadata = abjad.TypedOrderedDict([
     ('catalog_number', 'AWN-017'),
+    ('composer ', 'Bača'),
     ('forces_tagline', 'for piano'),
     ('title', 'Mráz'),
     ('year', 2017),
