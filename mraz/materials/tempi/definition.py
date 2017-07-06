e9114a4ee8f8b4848ca2c86893d1767db2ae6e84
Trevor Bača <trevor.baca@gmail.com>
2017-07-06 09:29:22 -0500

    Changed Tempo to MetronomeMark.

--- mraz/materials/tempi/definition.py
+++ mraz/materials/tempi/definition.py
@@ -5,10 +5,10 @@ import abjad
 tempi = abjad.TypedOrderedDict([
     (
         84,
-        abjad.Tempo(abjad.Duration(1, 4), 84),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 84),
         ),
     (
         112,
-        abjad.Tempo(abjad.Duration(1, 4), 112),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 112),
         ),
     ])
