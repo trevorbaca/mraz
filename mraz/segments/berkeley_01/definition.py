8878ddd06fbc2d3590565429efa0aca97813ad77
Trevor Bača <trevor.baca@gmail.com>
2017-08-03 17:21:39 -0500

    Travis commit.

--- mraz/segments/berkeley_01/definition.py
+++ mraz/segments/berkeley_01/definition.py
@@ -3,7 +3,6 @@ import abjad
 import baca
 import mraz
 
-raise Exception('REMOVE ME AFTER CONFIGURING TRAVIS')
 
 ###############################################################################
 ################################# [BERKELEY 1] ################################
