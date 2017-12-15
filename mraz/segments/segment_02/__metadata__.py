5b606addd68c9e143f67c8c5e01434a4dd89b03a
Trevor Bača <trevor.baca@gmail.com>
2017-12-14 18:37:56 -0600

    Cleaning up clef metadata.

--- mraz/segments/segment_02/__metadata__.py
+++ mraz/segments/segment_02/__metadata__.py
@@ -7,8 +7,14 @@ metadata = abjad.TypedOrderedDict(
             'end_clefs_by_context',
             abjad.TypedOrderedDict(
                 [
-                    ('PianoMusicLHStaff', 'bass'),
-                    ('PianoMusicRHStaff', 'treble'),
+                    (
+                        'PianoMusicLHStaff',
+                        ('bass', 'LHVoiceOne'),
+                        ),
+                    (
+                        'PianoMusicRHStaff',
+                        ('treble', 'RH Voice 1'),
+                        ),
                     ]
                 ),
             ),
