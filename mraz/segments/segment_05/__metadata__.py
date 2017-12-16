331c725c32af3f0c0ed0881cc5884a78e88cfaf4
Trevor Bača <trevor.baca@gmail.com>
2017-12-16 17:19:26 -0600

    Cleaned up metronome mark and instrument metadata.

--- mraz/segments/segment_05/__metadata__.py
+++ mraz/segments/segment_05/__metadata__.py
@@ -51,12 +51,25 @@ metadata = abjad.TypedOrderedDict(
             'end_instruments',
             abjad.TypedOrderedDict(
                 [
-                    ('PianoMusicStaffGroup', 'piano'),
+                    (
+                        'PianoMusicStaffGroup',
+                        ('piano', 'RHVoiceI'),
+                        ),
                     ]
                 ),
             ),
         ('end_margin_markup', None),
-        ('end_metronome_mark', '84'),
+        (
+            'end_metronome_marks',
+            abjad.TypedOrderedDict(
+                [
+                    (
+                        'Score',
+                        ('84', 'GlobalSkips'),
+                        ),
+                    ]
+                ),
+            ),
         ('end_staff_lines', None),
         ('first_measure_number', 132),
         ('segment_number', 8),
