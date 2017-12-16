638bbe18fd050ec28d54ad07f42b3c07efda1e48
Trevor Bača <trevor.baca@gmail.com>
2017-12-16 10:45:35 -0600

    Cleaning up metadata.

--- mraz/segments/berkeley_02/__metadata__.py
+++ mraz/segments/berkeley_02/__metadata__.py
@@ -4,7 +4,7 @@ import abjad
 metadata = abjad.TypedOrderedDict(
     [
         (
-            'end_clefs_by_context',
+            'end_clefs',
             abjad.TypedOrderedDict(
                 [
                     (
@@ -20,7 +20,7 @@ metadata = abjad.TypedOrderedDict(
             ),
         ('end_clock_time', "0'45''"),
         (
-            'end_dynamics_by_context',
+            'end_dynamics',
             abjad.TypedOrderedDict(
                 [
                     ('LHResonanceVoice', 'f'),
@@ -48,7 +48,7 @@ metadata = abjad.TypedOrderedDict(
                 ),
             ),
         (
-            'end_instruments_by_context',
+            'end_instruments',
             abjad.TypedOrderedDict(
                 [
                     ('PianoMusicStaffGroup', 'piano'),
@@ -57,7 +57,7 @@ metadata = abjad.TypedOrderedDict(
             ),
         ('end_metronome_mark', '84'),
         (
-            'end_staff_lines_by_staff',
+            'end_staff_lines',
             abjad.TypedOrderedDict(
                 []
                 ),
