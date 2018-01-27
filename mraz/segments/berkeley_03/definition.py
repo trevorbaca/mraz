7114a2677577b7116df6d984095f3801cbc00a4d
Trevor Bača <trevor.baca@gmail.com>
2018-01-27 11:32:28 -0600

    Changed 'spacing_specifier' to 'spacing'.

--- mraz/segments/berkeley_03/definition.py
+++ mraz/segments/berkeley_03/definition.py
@@ -990,7 +990,7 @@ metronome_mark_measure_map = baca.MetronomeMarkMeasureMap([
     (38, mraz.metronome_marks['84']),
     ])
 
-spacing_specifier = baca.HorizontalSpacingSpecifier(
+spacing = baca.HorizontalSpacingSpecifier(
     fermata_measure_width=(1, 4),
     minimum_width=(1, 24),
     )
@@ -1011,7 +1011,7 @@ maker = baca.SegmentMaker(
     rehearsal_letter='',
     score_template=mraz.ScoreTemplate(),
     skips_instead_of_rests=True,
-    #spacing_specifier=spacing_specifier,
+    #spacing=spacing,
     metronome_mark_measure_map=metronome_mark_measure_map,
     time_signatures=accumulator.time_signatures,
     )
