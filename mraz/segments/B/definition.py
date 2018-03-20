b45bfa8b1a904801579641765c2e8de1612fdfbf
Trevor Bača <trevor.baca@gmail.com>
2018-03-20 13:39:22 -0500

    Cleaned up definition files.

--- mraz/segments/B/definition.py
+++ mraz/segments/B/definition.py
@@ -289,14 +289,11 @@ maker = baca.SegmentMaker(
     color_out_of_range_pitches=True,
     color_repeat_pitch_classes=False,
     ignore_repeat_pitch_classes=True,
-    instruments=mraz.instruments,
     measures_per_stage=measures_per_stage,
-    metronome_marks=mraz.metronome_marks,
+    metronome_mark_measure_map=metronome_mark_measure_map,
     range_checker=abjad.Piano().pitch_range,
-    score_template=mraz.ScoreTemplate(),
     skips_instead_of_rests=True,
     spacing=spacing,
-    metronome_mark_measure_map=metronome_mark_measure_map,
     time_signatures=accumulator.time_signatures,
     )
 
