61b879ac6dd1a15f22594cf6b87035638b05fee0
Trevor Bača <trevor.baca@gmail.com>
2018-03-27 18:17:14 -0500

    Rebuilding score.

--- mraz/builds/arch-a-score/layout.py
+++ mraz/builds/arch-a-score/layout.py
@@ -35,3 +35,9 @@ breaks = baca.breaks(
         [105, 140, (15, 20)],
         ),
     )
+
+spacing = baca.scorewide_spacing(
+    __file__,
+    breaks=breaks,
+    fallback_duration=(1, 20),
+    )
