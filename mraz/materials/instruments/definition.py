6fbccfea35e23654240b598d7722b380d3319bc4
Trevor Bača <trevor.baca@gmail.com>
2018-03-30 11:39:01 -0500

    Cleaned up instrument dictionary.

--- mraz/materials/instruments/definition.py
+++ mraz/materials/instruments/definition.py
@@ -5,10 +5,6 @@ import baca
 instruments = abjad.OrderedDict([
     (
         'Piano',
-        abjad.Piano(
-            hide=True,
-            markup=baca.markup.instrument('Piano'),
-            short_markup=abjad.Markup.null(),
-            )
+        abjad.Piano(hide=True)
         ),
     ])
