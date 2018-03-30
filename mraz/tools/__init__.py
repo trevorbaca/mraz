db437f5100c63eacc00094fed65f5f51ff8de508
Trevor Bača <trevor.baca@gmail.com>
2018-03-30 13:19:10 -0500

    Cleaning up segments.

--- mraz/tools/__init__.py
+++ mraz/tools/__init__.py
@@ -5,6 +5,5 @@ from .ScoreTemplate import ScoreTemplate
 from .SilverDesignMaker import SilverDesignMaker
 from .clean_up_repeat_ties import clean_up_repeat_ties
 from .final_markup import final_markup
-from .hide_music import hide_music
 from .music_maker import music_maker
 from .transparent_music import transparent_music
