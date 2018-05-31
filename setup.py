cb68c1dfcbc6e6aef6c8a41966e446e87eef6e79
Trevor Bača <trevor.baca@gmail.com>
2018-05-31 14:09:15 -0500

    Cleaned up Travis configfile.

--- setup.py
+++ setup.py
@@ -2,14 +2,29 @@
 import setuptools
 
 
+install_requires = [
+    'abjad',
+    'mypy',
+    'roman',
+    ]
+
+keywords = [
+    'abjad',
+    'music composition',
+    'music notation',
+    'formalized score control',
+    'lilypond',
+    ]
+keywords = ', '.join(keywords)
+
 if __name__ == '__main__':
     setuptools.setup(
         author='Trevor Bača',
         author_email='trevor.baca@gmail.com',
-        install_requires=('abjad',),
-        name='mraz',
-        packages=('mraz',),
+        install_requires=install_requires,
+        keywords=keywords,
+        name='Mráz',
+        packages=['mraz'],
+        platforms='Any',
         url='https://github.com/trevorbaca/mraz',
-        version='0.1',
-        zip_safe=False,
         )
