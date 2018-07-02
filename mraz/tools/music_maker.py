7e477c8e381b1ec4d6ec26b2c4d42b5135863539
Trevor Bača <trevor.baca@gmail.com>
2018-07-02 17:43:48 -0500

    Updated doctests.

--- mraz/tools/music_maker.py
+++ mraz/tools/music_maker.py
@@ -61,29 +61,25 @@ def music_maker():
                     {
             <BLANKLINE>
                         % [GlobalSkips measure 1]                                          %! SM4
-                        \newSpacingSection                                                 %! HSS1:SPACING
-                        \set Score.proportionalNotationDuration = #(ly:make-moment 1 24)   %! HSS1:SPACING
+                        \baca_new_spacing_section #1 #24                                   %! HSS1:SPACING
                         \time 1/16                                                         %! SM8:EXPLICIT_TIME_SIGNATURE:SM1
                         \once \override Score.TimeSignature.color = #(x11-color 'blue)     %! SM6:EXPLICIT_TIME_SIGNATURE_COLOR:SM1
                         s1 * 1/16
             <BLANKLINE>
                         % [GlobalSkips measure 2]                                          %! SM4
-                        \newSpacingSection                                                 %! HSS1:SPACING
-                        \set Score.proportionalNotationDuration = #(ly:make-moment 1 24)   %! HSS1:SPACING
+                        \baca_new_spacing_section #1 #24                                   %! HSS1:SPACING
                         \time 7/16                                                         %! SM8:EXPLICIT_TIME_SIGNATURE:SM1
                         \once \override Score.TimeSignature.color = #(x11-color 'blue)     %! SM6:EXPLICIT_TIME_SIGNATURE_COLOR:SM1
                         s1 * 7/16
             <BLANKLINE>
                         % [GlobalSkips measure 3]                                          %! SM4
-                        \newSpacingSection                                                 %! HSS1:SPACING
-                        \set Score.proportionalNotationDuration = #(ly:make-moment 1 24)   %! HSS1:SPACING
+                        \baca_new_spacing_section #1 #24                                   %! HSS1:SPACING
                         \time 1/16                                                         %! SM8:EXPLICIT_TIME_SIGNATURE:SM1
                         \once \override Score.TimeSignature.color = #(x11-color 'blue)     %! SM6:EXPLICIT_TIME_SIGNATURE_COLOR:SM1
                         s1 * 1/16
             <BLANKLINE>
                         % [GlobalSkips measure 4]                                          %! SM4
-                        \newSpacingSection                                                 %! HSS1:SPACING
-                        \set Score.proportionalNotationDuration = #(ly:make-moment 1 24)   %! HSS1:SPACING
+                        \baca_new_spacing_section #1 #24                                   %! HSS1:SPACING
                         \time 6/16                                                         %! SM8:EXPLICIT_TIME_SIGNATURE:SM1
                         \once \override Score.TimeSignature.color = #(x11-color 'blue)     %! SM6:EXPLICIT_TIME_SIGNATURE_COLOR:SM1
                         s1 * 3/8
@@ -251,22 +247,19 @@ def music_maker():
                     {
             <BLANKLINE>
                         % [GlobalSkips measure 1]                                          %! SM4
-                        \newSpacingSection                                                 %! HSS1:SPACING
-                        \set Score.proportionalNotationDuration = #(ly:make-moment 1 24)   %! HSS1:SPACING
+                        \baca_new_spacing_section #1 #24                                   %! HSS1:SPACING
                         \time 9/16                                                         %! SM8:EXPLICIT_TIME_SIGNATURE:SM1
                         \once \override Score.TimeSignature.color = #(x11-color 'blue)     %! SM6:EXPLICIT_TIME_SIGNATURE_COLOR:SM1
                         s1 * 9/16
             <BLANKLINE>
                         % [GlobalSkips measure 2]                                          %! SM4
-                        \newSpacingSection                                                 %! HSS1:SPACING
-                        \set Score.proportionalNotationDuration = #(ly:make-moment 1 24)   %! HSS1:SPACING
+                        \baca_new_spacing_section #1 #24                                   %! HSS1:SPACING
                         \time 14/16                                                        %! SM8:EXPLICIT_TIME_SIGNATURE:SM1
                         \once \override Score.TimeSignature.color = #(x11-color 'blue)     %! SM6:EXPLICIT_TIME_SIGNATURE_COLOR:SM1
                         s1 * 7/8
             <BLANKLINE>
                         % [GlobalSkips measure 3]                                          %! SM4
-                        \newSpacingSection                                                 %! HSS1:SPACING
-                        \set Score.proportionalNotationDuration = #(ly:make-moment 1 24)   %! HSS1:SPACING
+                        \baca_new_spacing_section #1 #24                                   %! HSS1:SPACING
                         \time 8/16                                                         %! SM8:EXPLICIT_TIME_SIGNATURE:SM1
                         \once \override Score.TimeSignature.color = #(x11-color 'blue)     %! SM6:EXPLICIT_TIME_SIGNATURE_COLOR:SM1
                         s1 * 1/2
