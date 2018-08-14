a8e9f05596d83ae5ad2532cfb8fbf03b1e365ddf
Trevor Bača <trevor.baca@gmail.com>
2018-08-14 14:14:59 -0500

    Cleaned up text spanners.

--- mraz/segments/C/definition.py
+++ mraz/segments/C/definition.py
@@ -970,7 +970,7 @@ accumulator(
 
 maker = baca.SegmentMaker(
     color_octaves=False,
-    color_repeat_pitch_classes=False,
+    do_not_color_repeat_pitch_classes=True,
     final_bar_line=False,
     ignore_repeat_pitch_classes=True,
     segment_directory=abjad.Path(os.path.realpath(__file__)).parent,
