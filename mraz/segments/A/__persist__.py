c2c864b6f8fea87dc13121014c59d070030b615c
Trevor Bača <trevor.baca@gmail.com>
2019-02-26 10:20:14 -0600

    Cleaned up persist files.

--- mraz/segments/A/__persist__.py
+++ mraz/segments/A/__persist__.py
@@ -4,6 +4,41 @@ import abjad
 persist = abjad.OrderedDict(
     [
         (
+            'alive_during_segment',
+            [
+                'Score',
+                'Global_Context',
+                'Global_Rests',
+                'Global_Skips',
+                'Music_Context',
+                'Piano_Music_Staff_Group',
+                'Piano_Music_RH_Staff',
+                'RH_Voice_I',
+                'RH_Voice_I_Inserts',
+                'RH_Voice_II',
+                'RH_Voice_II_Inserts',
+                'RH_Voice_III',
+                'RH_Voice_III_Inserts',
+                'RH_Voice_IV',
+                'RH_Voice_IV_Inserts',
+                'RH_Voice_V',
+                'RH_Voice_VI',
+                'RH_Resonance_Voice',
+                'Piano_Music_LH_Staff',
+                'LH_Voice_I',
+                'LH_Voice_II',
+                'LH_Voice_III',
+                'LH_Voice_IV',
+                'LH_Voice_IV_Inserts',
+                'LH_Voice_V',
+                'LH_Voice_V_Inserts',
+                'LH_Voice_VI',
+                'LH_Rest_Voice_VI',
+                'LH_Voice_VI_Inserts',
+                'LH_Resonance_Voice',
+                ],
+            ),
+        (
             'persistent_indicators',
             abjad.OrderedDict(
                 [
