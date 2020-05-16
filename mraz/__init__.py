0496cee35e6fe9d1dc225762c5096b841b00955e
Trevor Bača <trevor.baca@gmail.com>
2020-05-16 17:15:03 -0400

    Sorted imports.

--- mraz/__init__.py
+++ mraz/__init__.py
@@ -1,6 +1,13 @@
 import distutils.version
 import platform
 
+from mraz.materials.instruments.definition import instruments
+from mraz.materials.metronome_marks.definition import metronome_marks
+from mraz.tools import *
+
+from .ScoreTemplate import ScoreTemplate
+from .SilverDesignMaker import SilverDesignMaker
+
 if not (
     distutils.version.LooseVersion("3.7")
     < distutils.version.LooseVersion(platform.python_version())
@@ -8,9 +15,3 @@ if not (
     raise ImportError("Requires Python 3.7.")
 del distutils
 del platform
-from .ScoreTemplate import ScoreTemplate
-from .SilverDesignMaker import SilverDesignMaker
-
-from mraz.tools import *
-from mraz.materials.instruments.definition import instruments
-from mraz.materials.metronome_marks.definition import metronome_marks
--- mraz/materials/metronome_marks/definition.py
+++ mraz/materials/metronome_marks/definition.py
@@ -1,6 +1,5 @@
 import abjad
 
-
 metronome_marks = abjad.OrderedDict(
     [
         ("84", abjad.MetronomeMark((1, 4), 84)),
