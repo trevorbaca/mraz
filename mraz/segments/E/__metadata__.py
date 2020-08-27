b4785fffed0464eb6cf808dacf6e1f260e728e43
Trevor Bača <trevor.baca@gmail.com>
2020-08-27 13:08:24 -0400

    Blacked code.

--- mraz/segments/E/__metadata__.py
+++ mraz/segments/E/__metadata__.py
@@ -4,19 +4,19 @@ import abjad
 metadata = abjad.OrderedDict(
     [
         (
-            'bol_measure_numbers',
+            "bol_measure_numbers",
             [78],
-            ),
-        ('duration', "0'09''"),
-        ('final_measure_number', 81),
-        ('first_measure_number', 78),
-        ('segment_name', 'E'),
-        ('segment_number', 5),
-        ('start_clock_time', "3'10''"),
-        ('stop_clock_time', "3'19''"),
+        ),
+        ("duration", "0'09''"),
+        ("final_measure_number", 81),
+        ("first_measure_number", 78),
+        ("segment_name", "E"),
+        ("segment_number", 5),
+        ("start_clock_time", "3'10''"),
+        ("stop_clock_time", "3'19''"),
         (
-            'time_signatures',
-            ['9/8', '4/8', '7/8', '5/8'],
-            ),
-        ]
-    )
+            "time_signatures",
+            ["9/8", "4/8", "7/8", "5/8"],
+        ),
+    ]
+)
