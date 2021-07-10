245825d278ca6aa8b609129ea0782ec2ed2620e3
Trevor Bača <trevor.baca@gmail.com>
2021-07-10 15:26:51 -0400

    Cleaned up fermata measure number metadata.

--- mraz/__metadata__.py
+++ mraz/__metadata__.py
@@ -4,29 +4,6 @@ metadata = abjad.OrderedDict(
     [
         ("catalog_number", "AWN-017"),
         ("composer ", "Trevor Bača"),
-        (
-            "fermata_measure_numbers",
-            abjad.OrderedDict(
-                [
-                    (
-                        "_",
-                        [4, 6, 9],
-                    ),
-                    (
-                        "B",
-                        [23, 25, 28],
-                    ),
-                    (
-                        "C",
-                        [54],
-                    ),
-                    (
-                        "E",
-                        [84, 88, 90, 97],
-                    ),
-                ]
-            ),
-        ),
         ("forces_tagline", "for piano"),
         ("title", "Mráz"),
         ("year", 2017),
