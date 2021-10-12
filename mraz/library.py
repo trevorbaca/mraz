4de79ecc6df95ae3d70846af1300e5059883931c
Trevor Bača <trevor.baca@gmail.com>
2021-10-12 11:24:41 +0200

    Cleaned up markup.

--- mraz/library.py
+++ mraz/library.py
@@ -308,7 +308,7 @@ class SilverDesignMaker:
                 part_segments = []
                 for i, part in enumerate(parts):
                     # string = rf"\concat {{ {source} \sub {i} }}"
-                    # markup = abjad.Markup(r"\markup {string}", literal=True)
+                    # markup = abjad.Markup(r"\markup {string}")
                     part_segment = baca.PitchClassSegment(part)
                     part_segments.append(part_segment)
                 new_segments.append(part_segments)
