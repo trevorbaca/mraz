ad4d036ce21283195a2dec07e9065f2c4620e0cd
Trevor Bača <trevor.baca@gmail.com>
2022-04-11 15:48:06 +0200

    Cleaned up selections.

--- mraz/sections/04/music.py
+++ mraz/sections/04/music.py
@@ -1115,7 +1115,7 @@ commands(
         baca.repeat_tie(
             baca.selectors.pleaves((1, None)),
         ),
-        map=baca.selectors.qruns(),
+        map=lambda _: baca.select.qruns(_),
     ),
 )
 
