1a9a4a95c06c7c2046027246fd86fde7069ec59a
Trevor Bača <trevor.baca@gmail.com>
2022-06-07 17:44:30 +0200

    Cleaned up music.py files.

--- mraz/sections/07/music.py
+++ mraz/sections/07/music.py
@@ -348,6 +348,7 @@ commands = baca.CommandAccumulator(
 
 baca.interpret.set_up_score(
     score,
+    commands,
     commands.manifests(),
     commands.time_signatures,
     append_anchor_skip=True,
@@ -369,10 +370,7 @@ for index, item in (
     indicator = commands.metronome_marks.get(item, item)
     baca.commands._metronome_mark(skip, indicator, manifests)
 
-commands(
-    "Skips",
-    baca.bar_line("|.", lambda _: baca.select.skip(_, -1)),
-)
+baca.commands._bar_line(score["Skips"][11 - 1], "|.")
 
 # reapply
 
