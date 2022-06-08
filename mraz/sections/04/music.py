4eeb2daffd7a5a74379de0e5b25039c47c64fb56
Trevor Bača <trevor.baca@gmail.com>
2022-06-08 10:41:50 +0200

    Cleaned up music.py files.

--- mraz/sections/04/music.py
+++ mraz/sections/04/music.py
@@ -1076,10 +1076,11 @@ for index, item in (
     indicator = commands.metronome_marks.get(item, item)
     baca.commands._metronome_mark(skip, indicator, manifests)
 
-commands(
-    "Rests",
-    baca.global_fermata("fermata", lambda _: abjad.select.leaf(_, 20)),
-)
+rests = score["Rests"]
+for index, string in (
+    (21 - 1, "fermata"),
+):
+    baca.global_fermata(rests[index], string)
 
 # reapply
 
--- mraz/sections/06/music.py
+++ mraz/sections/06/music.py
@@ -503,13 +503,14 @@ for index, item in (
     indicator = commands.metronome_marks.get(item, item)
     baca.commands._metronome_mark(skip, indicator, manifests)
 
-commands(
-    "Rests",
-    baca.global_fermata("short", lambda _: abjad.select.leaf(_, 2)),
-    baca.global_fermata("short", lambda _: abjad.select.leaf(_, 6)),
-    baca.global_fermata("short", lambda _: abjad.select.leaf(_, 8)),
-    baca.global_fermata("fermata", lambda _: abjad.select.leaf(_, -1)),
-)
+rests = score["Rests"]
+for index, string in (
+    (3 - 1, "short"),
+    (7 - 1, "short"),
+    (9 - 1, "short"),
+    (16 - 1, "fermata"),
+):
+    baca.global_fermata(rests[index], string)
 
 # reapply
 
