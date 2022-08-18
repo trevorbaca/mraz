5d4741ac432e5de2e46e139bac1e7f956f8beed3
Trevor Bača <trevor.baca@gmail.com>
2022-08-18 08:09:35 -0400

    Cleaned up music.py files.

--- mraz/sections/04/music.py
+++ mraz/sections/04/music.py
@@ -1394,8 +1394,7 @@ for index, item in (
     (36, "84"),
 ):
     skip = skips[index]
-    indicator = accumulator.metronome_marks.get(item, item)
-    baca.metronome_mark_function(skip, indicator, manifests)
+    baca.metronome_mark_function(skip, item, manifests)
 
 rests = score["Rests"]
 for index, string in ((21 - 1, "fermata"),):
