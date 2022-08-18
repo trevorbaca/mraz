171a42e668b651139f1d40bcfcc13fc9941d52ba
Trevor Bača <trevor.baca@gmail.com>
2022-08-18 09:16:08 -0400

    Cleaned up music.py files.

--- mraz/sections/07/music.py
+++ mraz/sections/07/music.py
@@ -471,8 +471,8 @@ baca.make_figures(
 voice_names = baca.accumulator.get_voice_names(score)
 
 accumulator = baca.CommandAccumulator(
-    instruments=library.instruments(),
-    metronome_marks=library.metronome_marks(),
+    instruments=library.instruments,
+    metronome_marks=library.metronome_marks,
     time_signatures=figures.time_signatures,
     voice_abbreviations=library.voice_abbreviations(),
     voice_names=voice_names,
@@ -481,7 +481,7 @@ accumulator = baca.CommandAccumulator(
 baca.interpret.set_up_score(
     score,
     accumulator,
-    accumulator.manifests(),
+    library.manifests,
     accumulator.time_signatures,
     append_anchor_skip=True,
     always_make_global_rests=True,
@@ -491,7 +491,7 @@ baca.interpret.set_up_score(
 figures.populate_commands(score, accumulator)
 
 skips = score["Skips"]
-manifests = accumulator.manifests()
+manifests = library.manifests
 
 for index, item in (
     (0, "84"),
@@ -577,7 +577,7 @@ def main():
     ]
     baca.reapply(
         accumulator.voices(music_voice_names),
-        accumulator.manifests(),
+        library.manifests,
         previous_persistent_indicators,
     )
     cache = baca.interpret.cache_leaves(
@@ -594,7 +594,7 @@ if __name__ == "__main__":
     del defaults["check_wellformedness"]
     metadata, persist, score, timing = baca.build.section(
         score,
-        accumulator.manifests(),
+        library.manifests,
         accumulator.time_signatures,
         **defaults,
         activate=(baca.tags.LOCAL_MEASURE_NUMBER,),
