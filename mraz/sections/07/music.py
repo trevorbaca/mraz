0dc1664ce372385768e17fd581c3a9de6cda5b86
Trevor Bača <trevor.baca@gmail.com>
2022-08-19 09:21:58 -0400

    Cleaned up music.py files.

--- mraz/sections/07/music.py
+++ mraz/sections/07/music.py
@@ -590,15 +590,14 @@ def main():
 
 if __name__ == "__main__":
     main()
-    defaults = baca.interpret.section_defaults()
-    del defaults["check_wellformedness"]
     metadata, persist, timing = baca.build.section(
         score,
         library.manifests,
         accumulator.time_signatures,
-        **defaults,
+        **baca.interpret.section_defaults(),
         activate=(baca.tags.LOCAL_MEASURE_NUMBER,),
         always_make_global_rests=True,
+        do_not_check_wellformedness=True,
         do_not_require_short_instrument_names=True,
         error_on_not_yet_pitched=True,
         final_section=True,
