2c4c3765d4c5948102635381ff0edd8d6f0cbc97
Trevor Bača <trevor.baca@gmail.com>
2022-09-26 11:16:54 -0400

    Cleaned up environment handling.

--- mraz/sections/06/music.py
+++ mraz/sections/06/music.py
@@ -625,10 +625,10 @@ def make_empty_score(first_measure_number, previous_persistent_indicators):
         score,
         accumulator.time_signatures,
         accumulator,
-        library.manifests,
         append_anchor_skip=True,
         always_make_global_rests=True,
         first_measure_number=first_measure_number,
+        manifests=library.manifests,
         previous_persistent_indicators=previous_persistent_indicators,
     )
     figures.populate_commands(score, accumulator)
@@ -713,7 +713,7 @@ def make_score(first_measure_number, previous_persistent_indicators):
         or "InsertVoice" in _
         or "ResonanceVoice" in _
     ]
-    baca.reapply(
+    baca.section.reapply(
         accumulator.voices(music_voice_names),
         library.manifests,
         previous_persistent_indicators,
@@ -735,17 +735,18 @@ def main():
         environment.previous_persist["persistent_indicators"],
         timing,
     )
-    metadata, persist, timing = baca.build.postprocess_score(
+    metadata, persist = baca.section.postprocess_score(
         score,
-        library.manifests,
         accumulator.time_signatures,
-        environment,
         **baca.section.section_defaults(),
         activate=[baca.tags.LOCAL_MEASURE_NUMBER],
         always_make_global_rests=True,
         do_not_check_wellformedness=True,
         do_not_require_short_instrument_names=True,
+        environment=environment,
         error_on_not_yet_pitched=True,
+        manifests=library.manifests,
+        timing=timing,
     )
     lilypond_file = baca.lilypond.file(
         score,
