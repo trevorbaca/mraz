d02fe2857fd71012aac0e34a185ddeb2c844c3f4
Trevor Bača <trevor.baca@gmail.com>
2022-09-27 10:25:52 -0400

    Cleaned up metadata.

--- mraz/sections/04/music.py
+++ mraz/sections/04/music.py
@@ -1463,8 +1463,8 @@ def main():
     )
     baca.build.persist_lilypond_file(
         lilypond_file,
-        environment.metadata,
-        environment.persist,
+        metadata,
+        persist,
         timing,
         environment.arguments,
     )
