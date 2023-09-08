5346b7ac3be0611070b68e6475d3c15eb35e06ef
Trevor Bača <trevor.baca@gmail.com>
2023-09-08 13:15:58 -0400

    Cleaned up layout.py files.

--- mraz/sections/02/layout.py
+++ mraz/sections/02/layout.py
@@ -1,15 +1,19 @@
 import baca
 
-spacing = baca.make_layout(
-    baca.page(
-        1,
-        baca.system(measure=1, y_offset=0, distances=(10, 15)),
-        baca.system(measure=4, y_offset=60, distances=(10, 15)),
-        baca.system(measure=8, y_offset=120, distances=(10, 15)),
-        baca.system(measure=12, y_offset=180, distances=(10, 15)),
-    ),
-    spacing=(1, 32),
-)
+
+def main():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=0, distances=(10, 15)),
+            baca.system(measure=4, y_offset=60, distances=(10, 15)),
+            baca.system(measure=8, y_offset=120, distances=(10, 15)),
+            baca.system(measure=12, y_offset=180, distances=(10, 15)),
+        ),
+        spacing=(1, 32),
+    )
+    baca.section.make_layout_ly(spacing)
+
 
 if __name__ == "__main__":
-    baca.section.make_layout_ly(spacing)
+    main()
