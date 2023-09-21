ec003d4da784492052f8e2aae7ae975ffafd17a7
Trevor Bača <trevor.baca@gmail.com>
2023-09-21 12:42:53 -0400

    Cleaned up music.py files.

--- mraz/sections/07/music.py
+++ mraz/sections/07/music.py
@@ -714,5 +714,17 @@ def main():
     )
 
 
+def make_layout():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=0, distances=(22, 22)),
+            baca.system(measure=6, y_offset=85, distances=(25, 22)),
+        ),
+        spacing=(1, 48),
+    )
+    baca.section.make_layout_ly(spacing)
+
+
 if __name__ == "__main__":
     main()
