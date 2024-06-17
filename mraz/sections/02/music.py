b07da5f0f0317077a58f01d0d5ee9b75c44f7351
Trevor Bača <trevor.baca@gmail.com>
2024-06-17 14:42:27 -0400

    Cleaned up layout files.

--- mraz/sections/02/music.py
+++ mraz/sections/02/music.py
@@ -375,7 +375,7 @@ def persist_score(score, environment):
     )
 
 
-def make_layout():
+def make_layout(environment):
     breaks = baca.layout.Breaks(
         baca.layout.Page(
             1,
@@ -388,7 +388,12 @@ def make_layout():
     spacing = baca.layout.Spacing(
         default=(1, 32),
     )
-    baca.build.write_layout_ily(breaks, spacing)
+    baca.build.write_layout_ily(
+        breaks,
+        environment.metadata["time_signatures"],
+        spacing,
+        first_measure_number=environment.first_measure_number,
+    )
 
 
 def main():
@@ -401,7 +406,7 @@ def main():
         )
         persist_score(score, environment)
     if environment.arguments.layout:
-        make_layout()
+        make_layout(environment)
 
 
 if __name__ == "__main__":
--- mraz/sections/06/music.py
+++ mraz/sections/06/music.py
@@ -853,7 +853,7 @@ def persist_score(score, environment):
     )
 
 
-def make_layout():
+def make_layout(environment):
     breaks = baca.layout.Breaks(
         baca.layout.Page(
             1,
@@ -876,7 +876,12 @@ def make_layout():
             baca.layout.Override([3, 7, 9, 16], (1, 4)),
         ),
     )
-    baca.build.write_layout_ily(breaks, spacing)
+    baca.build.write_layout_ily(
+        breaks,
+        environment.metadata["time_signatures"],
+        spacing,
+        first_measure_number=environment.first_measure_number,
+    )
 
 
 def main():
@@ -889,7 +894,7 @@ def main():
         )
         persist_score(score, environment)
     if environment.arguments.layout:
-        make_layout()
+        make_layout(environment)
 
 
 if __name__ == "__main__":
