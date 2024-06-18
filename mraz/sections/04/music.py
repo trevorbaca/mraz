ab9b080475bf052e581db25480d6ff5c03b12fd3
Trevor Bača <trevor.baca@gmail.com>
2024-06-18 13:26:22 -0400

    Cleaned up layout.py files.

--- mraz/sections/04/music.py
+++ mraz/sections/04/music.py
@@ -1660,7 +1660,7 @@ def make_layout(environment):
             baca.layout.Override((37, 38), (1, 32)),
         ),
     )
-    baca.build.write_layout_ily(
+    lilypond_file, bol_measure_numbers = baca.build.write_layout_ily(
         breaks,
         environment.metadata["time_signatures"],
         fermata_measure_numbers=environment.metadata.get("fermata_measure_numbers", []),
